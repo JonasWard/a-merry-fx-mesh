40e18b345de97a84a5d12762711c0976b8e24104
JonasWard <43600893+JonasWard@users.noreply.github.com>
2024-12-24 00:06:17 +0100

    feat: abstracted some methods

--- src/Shaders/tpmsCircles.glsl
+++ src/Shaders/tpmsCircles.glsl
@@ -3,9 +3,13 @@ const float BUBBLE_POWER = 0.5;
 const float DROP_RADIUS = 0.05;
 const vec2 DROP_DIRECTION = vec2(-1.0, -1.0) * DROP_RADIUS;
 
+float pDistance(vec2 p0, vec2 p1) {
+  vec2 d = p1 - p0;
+  return pow(dot(d,d),0.5);
+}
+
 float sdCircle(vec3 c, vec2 p) {
-  vec2 d = c.xy - p;
-  return pow(dot(d,d),0.5) - c.z;
+  return pDistance(c.xy, p) - c.z;
 }
 
 float sdLine(vec2 a, vec2 d, vec2 p) {
@@ -21,6 +25,11 @@ float sdCircleSharp(vec3 c, vec2 p) {
   } else {
     return 1.0;
   }
+}
+
+vec3 rotateCircleAroundCenter(vec3 c) {
+  float a = ROTATION_VELOCITY * ( (uTime + (1.0 + 0.5 * sin(pow(dot(c.xy,c.xy),0.5) * ANGLE_MULTIPLIER)))) * c.z;
+  return vec3(c.x * CENTER_OFFSET_MULTIPLIER + c.x * cos(a) - c.y * sin(a), c.y * CENTER_OFFSET_MULTIPLIER + c.x * sin(a) + c.y * cos(a), c.z); 
 }
 
 float sdCircleBubble(vec3 c, vec2 p) {
