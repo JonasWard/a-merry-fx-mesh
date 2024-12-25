f987d81a8c5e2cf5f7903e1b4231771453071948
JonasWard <43600893+JonasWard@users.noreply.github.com>
2024-12-26 00:35:50 +0100

    chore: tweaked default values

--- src/modelDefinition/types/version0.ts
+++ src/modelDefinition/types/version0.ts
@@ -6,22 +6,22 @@ import { SDFMainMethodLabels } from './methodSemantics';
 const dotsMethodVersionStack: ArrayEntryDataType = [
   [1, 3],
   [
-    DataEntryFactory.createEnum(0, SDFMainMethodLabels.length - 1, AttributeNames.SDFMethod),
-    DataEntryFactory.createFloat(1, 0.001, 1000, 3, AttributeNames.MethodScale),
+    DataEntryFactory.createEnum(4, SDFMainMethodLabels.length - 1, AttributeNames.SDFMethod),
+    DataEntryFactory.createFloat(0.025, 0.001, 1000, 3, AttributeNames.MethodScale),
   ],
 ];
 
 const mainMethods: EnumEntryDataType = [
   0,
   [
-    DataEntryFactory.createInt(5, 1, 100, `count`),
+    DataEntryFactory.createInt(100, 1, 100, `count`),
     DataEntryFactory.createFloat(10, 1, 500, 0, `minSize`),
     DataEntryFactory.createFloat(10, 1, 500, 0, `maxSize`),
-    DataEntryFactory.createFloat(0.05, 0.0, 1, 3, 'sinAmplitude'),
+    DataEntryFactory.createFloat(0.05, 0, 1, 3, 'sinAmplitude'),
     DataEntryFactory.createInt(0, 0, 1023, `seed`),
     DataEntryFactory.createFloat(0.01, 0.001, 0.1, 3, `rotationSpeed`),
     DataEntryFactory.createFloat(10, 1, 100, 1, `angleMultiplier`),
-    DataEntryFactory.createFloat(0.0, -5, 5, 2, `centerOffsetMultiplier`),
+    DataEntryFactory.createFloat(-0.5, -5, 5, 2, `centerOffsetMultiplier`),
     DataEntryFactory.createFloat(0.1, 0.1, 50, 1, `edgeThickness`),
   ],
   [
