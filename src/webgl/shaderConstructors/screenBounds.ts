1cb620f84487d09131f9b0fb5fb7c839b8d50a8f
JonasWard <43600893+JonasWard@users.noreply.github.com>
2024-12-27 00:31:57 +0100

    fix: increase font weight a bit

--- src/webgl/shaderConstructors/screenBounds.ts
+++ src/webgl/shaderConstructors/screenBounds.ts
@@ -1,4 +1,4 @@
 export const MAX_HEIGHT = Math.max(window.innerHeight, window.innerWidth) * 1.15;
 export const MAX_WIDTH = Math.max(window.innerHeight, window.innerWidth) * 1.15;
-export const MAX_TEXT_WIDTH = Math.min(500, Math.min(window.innerHeight, window.innerWidth) * 0.7);
+export const MAX_TEXT_WIDTH = Math.min(500, Math.min(window.innerHeight, window.innerWidth) * 0.8);
 export const FONT_SIZE = MAX_TEXT_WIDTH * 0.1;
