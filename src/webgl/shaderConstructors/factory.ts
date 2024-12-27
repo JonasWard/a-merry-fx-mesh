daa738754e98cbb945846c74f8c389efaf0a617b
JonasWard <43600893+JonasWard@users.noreply.github.com>
2024-12-27 13:00:50 +0100

    feat: encapsulating shader generation method

--- src/webgl/shaderConstructors/factory.ts
+++ src/webgl/shaderConstructors/factory.ts
@@ -13,29 +13,34 @@ void main() {
 
 export const getFragmentShader = (data: Version0Type): string => {
   let shader = simpleUVShader;
-  switch (data['Main Methods'].s.value) {
-    case 0: // circle
-      shader = getCircleFragmentShader(
-        (data['Main Methods'].v as any).count.value,
-        (data['Main Methods'].v as any).minSize.value,
-        (data['Main Methods'].v as any).maxSize.value,
-        (data['Main Methods'].v as any).seed.value,
-        (data['Main Methods'].v as any).sinAmplitude.value,
-        getColor(data.Material['Normal Material']['color 0']),
-        getColor(data.Material['Normal Material']['color 1']),
-        (data['Main Methods'].v as any).rotationSpeed.value,
-        (data['Main Methods'].v as any).angleMultiplier.value,
-        (data['Main Methods'].v as any).centerOffsetMultiplier.value,
-        (data['Main Methods'].v as any).edgeThickness.value
-      );
-      break;
-    case 1: // dots
-      shader = getDotsFragmentShader(data);
-      break;
-    case 5: // drei eck
-      shader = getDreiEckFragmentShader(data);
-      break;
-    default:
+  try {
+    switch (data['Main Methods'].s.value) {
+      case 0: // circle
+        shader = getCircleFragmentShader(
+          (data['Main Methods'].v as any).count.value,
+          (data['Main Methods'].v as any).minSize.value,
+          (data['Main Methods'].v as any).maxSize.value,
+          (data['Main Methods'].v as any).seed.value,
+          (data['Main Methods'].v as any).sinAmplitude.value,
+          getColor(data.Material['Normal Material']['color 0']),
+          getColor(data.Material['Normal Material']['color 1']),
+          (data['Main Methods'].v as any).rotationSpeed.value,
+          (data['Main Methods'].v as any).angleMultiplier.value,
+          (data['Main Methods'].v as any).centerOffsetMultiplier.value,
+          (data['Main Methods'].v as any).edgeThickness.value
+        );
+        break;
+      case 1: // dots
+        shader = getDotsFragmentShader(data);
+        break;
+      case 5: // drei eck
+        shader = getDreiEckFragmentShader(data);
+        break;
+      default:
+    }
+  } catch (e) {
+    console.error('something went wrong when trying to create the shader');
+    console.warn(e);
   }
   if (import.meta.env.DEV) console.log(shader);
   return shader;
